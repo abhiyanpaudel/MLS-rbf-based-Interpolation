b4b2537b758218557de755d325866fb6908b8bc4
Abhiyan Paudel <paudea@yahtzee.scorec.rpi.edu>
2024-07-02 18:38:15 -0400

    original

--- MLSInterpolation.hpp
+++ MLSInterpolation.hpp
@@ -3,99 +3,88 @@
 
 #include "points.hpp"
 #include "MLSCoefficients.hpp"
+#include "adj_search.hpp"
+
+Write<Real> mls_interpolation(const Reals source_coordinates, const Reals target_coordinates, const SupportResults& support, const LO& dim, const Real& cutoff_radius){
+    const auto nvertices_source = source_coordinates.size();    
+    const auto nvertices_target = target_coordinates.size();
+
+    Write<Real>  approx_target_values(nvertices_target, 0, "approximated target values");
 
 
+    Kokkos::parallel_for("MLS coefficients", team_policy(nvertices_target, Kokkos::AUTO), KOKKOS_LAMBDA(const member_type& team){
 
-void mls_interpolation(DevicePoints device_source_data, DevicePoints device_target_data, DeviceRealVecView d_source_exact_sol, RealVecView approx_target_values , double cutoff_radius){
-    
-     int nTar = device_target_data.coordinates.extent(0);
-     int nSrc = device_source_data.coordinates.extent(0);
+	int i = team.league_rank();
+         
+	int start_ptr = support.supports_ptr[i];
+	int end_ptr = support.supports_ptr[i+1];
 
+
+	int nsupports = end_ptr - start_ptr;
+
+	DevicePoints local_source_points;
+	 
+        local_source_points.coordinates = DevicePointsViewType("Number of local source supports", nsupports);
+
+     for (int j = start_ptr; j < end_ptr; ++j){
+	auto index = support.supports_idx[j];
+	local_source_points.coordinates(j).x = source_coordinates[index * dim];
+        local_source_points.coordinates(j).y = source_coordinates[index * dim + 1];
+
+     }
+
+//	Real lower[6][6];
+//	Real forward_matrix[6][6];
+//	Real moment_matrix[6][6];
+//	Real inverse_matrix[6][6];
+//	Real targetMonomialVec[6];
+//
+	RealMatViewRight lower("Lower triangular matrix", 6,6);                                                                                                                                                            
      
-     RealMatViewRight lower("Lower triangular matrix", 6,6);                                                                                                                                                            
-     
-     RealMatViewRight forward_matrix("matrix from forward substitution", 6,6);  
+        RealMatViewRight forward_matrix("matrix from forward substitution", 6,6);  
        
 
-     RealMatViewRight moment_matrix("moment matrix P^T.phi.p", 6, 6);
+        RealMatViewRight moment_matrix("moment matrix P^T.phi.p", 6, 6);
  
 
-     RealMatViewRight inverse_matrix("inverse of  P^T.phi.P", 6, 6);
+        RealMatViewRight inverse_matrix("inverse of  P^T.phi.P", 6, 6);
 
 
-     RealVecView targetMonomialVec("Monomial Vector", 6);
-     
-     Kokkos::parallel_for("MLS coefficients", team_policy(nTar, Kokkos::AUTO), KOKKOS_LAMBDA(const member_type& team){
+        RealVecView targetMonomialVec("Monomial Vector", 6);
 
-     int i = team.league_rank();
-      
-     TargetToSourceMap target_to_source_map;
-      int nsupports = 0;
-      Kokkos::parallel_reduce(Kokkos::TeamThreadRange(team, nSrc), [=](int j, int& inner_count) {
-         double dist = distance(device_target_data.coordinates(i), device_source_data.coordinates(j));
-            if (dist < cutoff_radius) {
-                inner_count++;
-            }
-        },nsupports);
-    
-    Kokkos::fence();    
+
+	
+	RealMatViewRight V("Vandermonde matrix initialization", nsupports,6);
+   
+	RealVecView Phi("Phi Components", nsupports);
+
+	RealMatViewRight Ptphi("PtPhi matrix multiplication", 6, nsupports);   
+   
+   
+	RealMatViewRight resultant_matrix("inverse matrix times ptPhi", 6, nsupports);
+   
   
-    target_to_source_map.source_ids = IntVecView("SourceIds", nsupports);
+	RealVecView exactSupportValues("exact values within supports", nsupports);
+   
 
+	RealVecView result("Kernel vector", nsupports);                                                       
+   
     
-        int count = 0;
-        Kokkos::parallel_for(Kokkos::TeamThreadRange(team, nSrc), [&](int j) {
-            double dist = distance(device_target_data.coordinates(i), device_source_data.coordinates(j));
-            if (dist < cutoff_radius) {
-                int idx = Kokkos::atomic_fetch_add(&count,1); 
-                target_to_source_map.source_ids(idx-1) = j;
-            }
-        });
- 
-
-    Kokkos::fence();
- 
-     DevicePoints local_source_points;
-     
- 
-     local_source_points.coordinates = DevicePointsViewType("Number of local source supports", nsupports);
-
-     for (int j = 0; j < nsupports; ++j){
-	int source_idx = target_to_source_map.source_ids(j);     
-	local_source_points.coordinates(j).x = device_source_data.coordinates(source_idx).x;
-	local_source_points.coordinates(j).y = device_source_data.coordinates(source_idx).y;	
-
-     }	     
-     
-     RealMatViewRight V("Vandermonde matrix initialization", nsupports,6);
-      
-     RealVecView Phi("Phi Components", nsupports);
-
-     RealMatViewRight Ptphi("PtPhi matrix multiplication", 6, nsupports);   
+	Coord target_point;
     
-     
-     RealMatViewRight resultant_matrix("inverse matrix times ptPhi", 6, nsupports);
-     
-  
-     RealVecView exactSupportValues("exact values within supports", nsupports);
     
-
-     RealVecView result("Kernel vector", nsupports);                                                       
-    
-        
-     Coord target_point;
-        
-        
-     target_point.x = device_target_data.coordinates(i).x;
-      
-     target_point.y = device_target_data.coordinates(i).y;
+	target_point.x = target_coordinates[i*dim];
+   
+	target_point.y = target_coordinates[i*dim + 1];
 
 
-     BasisPoly(targetMonomialVec, target_point);
-     
+	BasisPoly(targetMonomialVec, target_point);
+   
     
-       Kokkos::parallel_for(Kokkos::TeamThreadRange(team, nsupports), [=](int j){ 
-           VandermondeMatrix(V, local_source_points, j);
+        Kokkos::parallel_for(Kokkos::TeamThreadRange(team, nsupports), [=](int j){ 
+       
+
+	   VandermondeMatrix(V, local_source_points, j);
            team.team_barrier();
            PhiVector (Phi,target_point, local_source_points, j, cutoff_radius);
            team.team_barrier();
@@ -104,7 +93,7 @@ void mls_interpolation(DevicePoints device_source_data, DevicePoints device_targ
              
        });
 
-        MatMatMul(team, moment_matrix, Ptphi, V);
+	MatMatMul(team, moment_matrix, Ptphi, V);
         Kokkos::fence(); 
         cholesky_decomposition( team, moment_matrix , lower);   
 	Kokkos::fence();
@@ -116,12 +105,16 @@ void mls_interpolation(DevicePoints device_source_data, DevicePoints device_targ
        MatMatMul( team, resultant_matrix, inverse_matrix, Ptphi); 
        Kokkos::fence();	
        MatVecMul(team, targetMonomialVec, resultant_matrix,result);
-       Kokkos::fence(); 
+       Kokkos::fence();
+	Kokkos::parallel_for(Kokkos::TeamThreadRange(team, nsupports), [=](const int i){
+	    exactSupportValues(i) = func(local_source_points.coordinates(i));
+	});
+
         double tgt_value = 0; 
         dot_product(team, result, exactSupportValues, tgt_value);
         
        if (team.team_rank() == 0){        
-           approx_target_values(i) = tgt_value;
+           approx_target_values[i] = tgt_value;
         }
    }); 
 
